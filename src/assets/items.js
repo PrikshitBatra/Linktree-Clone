c518ca12915f720aa97ad28e68bb74799a314595
Prikshit Batra <prikshit_batra@MacBook-Pro-3.local>
2023-01-11 03:01:48 +0530

    :)

---
+++ src/assets/items.js
@@ -0,0 +1,49 @@
+// import website from '../assets/website.png'
+import instagram from '../assets/instagram.png'
+// import appstore from '../assets/playstore.png'
+// import youtube from '../assets/youtube.jpeg'
+// import dribbble from '../assets/dribbble.png'
+import github from '../assets/github.png'
+//import telegram from '../assets/telegram.png'
+import linkedin from '../assets/linkedin.png'
+import Music from '../assets/Music.png'
+// import personallogo from '../assets/personalsite.png'
+import twitter from '../assets/twitter.png'
+
+
+const items = [
+// {
+
+{
+    "title": "Music",
+    "subtitle": "@prikshit-batra | Follow me",
+    "image": Music,
+    "link": "https://music.apple.com/profile/pbrick" //Linkedin Profile link
+},
+{
+    "title": "Linkedin",
+    "subtitle": "@prikshit-batra | Connect with me",
+    "image": linkedin,
+    "link": "https://www.linkedin.com/in/prikshit-batra/" //Linkedin Profile link
+},
+{
+    "title": "Instagram",
+    "subtitle": "@prikshit.batra | Follow me",
+    "image": instagram,
+    "link": "https://www.instagram.com/prikshit.batra/" //instagram profile link 
+},
+{
+    "title": "GitHub",
+    "subtitle": "@PrikshitBatra",
+    "image": github,
+    "link": "https://github.com/PrikshitBatra" //Github Profile link
+},
+{
+    "title": "Twitter",
+    "subtitle": "@PrikshitBatra",
+    "image": twitter,
+    "link": "https://twitter.com/PrikshitBatra" //Twitter Profile link
+},
+]
+
+export default items
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+//import * as Sentry from "@sentry/react";
+//import { Integrations } from "@sentry/tracing";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// Sentry.init({
+//   dsn: "https://4aa3698b34584e548f3a2ca4d1369c2b@o1080390.ingest.sentry.io/6169629",
+//   integrations: [new Integrations.BrowserTracing()],
+
+//   // Set tracesSampleRate to 1.0 to capture 100%
+//   // of transactions for performance monitoring.
+//   // We recommend adjusting this value in production
+//   tracesSampleRate: 1.0,
+// });
+
+ReactDOM.render( < React.StrictMode >
     <App />
-  </React.StrictMode>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
